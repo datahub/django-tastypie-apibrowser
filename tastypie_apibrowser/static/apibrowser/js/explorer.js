4e44d8bf8a8d32be0591f640aed8453f993ed2ab
Allan James Vestal <allanjamesvestal@gmail.com>
2014-02-17 20:10:17 -0600

    Javascript scope tweaking.

--- tastypie_apibrowser/static/apibrowser/js/explorer.js
+++ tastypie_apibrowser/static/apibrowser/js/explorer.js
@@ -1,131 +1,131 @@
 /**
- * 
+ *
  * This is an API explorer for tastypie
- * 
+ *
  * It relies on jQuery and underscore.js
- * 
+ *
  */
 
 APIExplorer = null;
-(function () { 
-    
-    var default_config = { 
+(function () {
+
+    var default_config = {
         base_endpoint: '/api/v1/'
     };
 
-    function Livingstone(config) { 
+    function Livingstone(config) {
         var combined_config = default_config;
-        _.each(config, function(value, key, config) { 
+        _.each(config, function(value, key, config) {
             combined_config[key] = config[key];
         });
         Livingstone.prototype.init(combined_config);
     }
-    
+
     Livingstone.Models = {};
     Livingstone.Views = {};
     Livingstone.Originals = {};
 
-    Livingstone.Models.Application = function(label, end_point) { 
+    Livingstone.Models.Application = function(label, end_point) {
         this.label = label; this.end_point = end_point;
     };
 
-    Livingstone.Views.APIView = function(data) { 
+    Livingstone.Views.APIView = function(data) {
         this.template = '#endpoint-panel-template';
         this.data=data;
     };
 
-    Livingstone.prototype = { 
+    Livingstone.prototype = {
 
-        Applications: {}, 
-        
+        Applications: {},
+
         template: "#explorer-template",
-        
-        init: function(config) { 
+
+        init: function(config) {
             _.bindAll(this);
-            this.$el = $(config['element']);
+            this.$el = django.jQuery(config['element']);
             this.options = config;
             var t = this;
             this.get_application_list(function() {
                 t.render();
-                Livingstone.Originals['endpoint-panel'] = 
-                                        $("#endpoint-panel").html();
+                Livingstone.Originals['endpoint-panel'] =
+                                        django.jQuery("#endpoint-panel").html();
 
-                Livingstone.Originals['schema-display'] = 
-                                        $("#schema-display").html();
+                Livingstone.Originals['schema-display'] =
+                                        django.jQuery("#schema-display").html();
             });
         },
 
-        get_application_list: function(callback) { 
+        get_application_list: function(callback) {
             var t= this;
-            $.get(this.options['base_endpoint'], function(response) { 
-                _.each(response.objects, function(v,k,l) { 
+            django.jQuery.get(this.options['base_endpoint'], function(response) {
+                _.each(response.objects, function(v,k,l) {
                     t.Applications[v.application] = v.uri;
                 });
-                if (callback !== null && 
-                    typeof callback == 'function') {  
+                if (callback !== null &&
+                    typeof callback == 'function') {
                     callback();
                 }
             });
         },
 
-        get_application_api: function(app_url, callback) { 
+        get_application_api: function(app_url, callback) {
             var t = this;
-            $.get(app_url, function (response) { 
-                if (typeof callback == 'function') { 
+            django.jQuery.get(app_url, function (response) {
+                if (typeof callback == 'function') {
                     callback(response);
                 }
             });
         },
 
-        show_schema: function(api_data) { 
-            $("#endpoint-panel").html(
-                _.template($('#endpoint-panel-template').html(), 
+        show_schema: function(api_data) {
+            django.jQuery("#endpoint-panel").html(
+                _.template(django.jQuery('#endpoint-panel-template').html(),
                 {schema:api_data}));
-            
+
         },
 
-        show_model_list: function(api_data) { 
+        show_model_list: function(api_data) {
             var t = this;
-            $("#schema-display").html(
+            django.jQuery("#schema-display").html(
                 _.template(
-                    $("#schema-display-template").html(),
+                    django.jQuery("#schema-display-template").html(),
                     {
                         models: api_data
                     }
-                )); 
+                ));
 
-            $("#endpoint-panel").html(
+            django.jQuery("#endpoint-panel").html(
                 Livingstone.Originals['endpoint-panel']);
 
-            $("#schema-display a").click(function(evt) { 
-                var schema_url = $(this).attr('href').substr(1);
+            django.jQuery("#schema-display a").click(function(evt) {
+                var schema_url = django.jQuery(this).attr('href').substr(1);
                 t.get_application_api(schema_url, t.show_schema);
             });
         },
 
-        render: function() { 
+        render: function() {
             var t = this;
-            this.$el.append (  
-                _.template($(this.template).html(), 
-                { 
+            this.$el.append (
+                _.template(django.jQuery(this.template).html(),
+                {
                     applications: this.Applications,
                     appkeys: _.sortBy(
-                                _.keys(this.Applications), 
+                                _.keys(this.Applications),
                                 function(key) { return key; }
                             )
                 }));
 
-            $("#application-list a").click(function(evt) { 
-                var app_url = $(this).attr('href').substr(1);
+            django.jQuery("#application-list a").click(function(evt) {
+                var app_url = django.jQuery(this).attr('href').substr(1);
                 t.get_application_api(app_url, t.show_model_list);
 
-                $("#endpoint-panel").html(
+                django.jQuery("#endpoint-panel").html(
                     Livingstone.Originals['endpoint-panel']);
 
             });
         }
     };
-     
+
     APIExplorer = Livingstone;
 
 })();
