8953cf04fc83bf91750c32d4455fb6b133760ffb
Allan James Vestal <allanjamesvestal@gmail.com>
2014-02-17 20:04:51 -0600

    Removed staff member view entirely. Just uses class-based view now.

--- tastypie_apibrowser/urls.py
+++ tastypie_apibrowser/urls.py
@@ -4,10 +4,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.views.generic import TemplateView
 
 
-# Imports from this app
-# from views import StaffMemberView
-
-
 urlpatterns = patterns('',
         url(r'^$', staff_member_required(TemplateView.as_view(template_name='apibrowser/index.html'))),
         url(r'^templates$', TemplateView.as_view(template_name='apibrowser/templates.html')),
