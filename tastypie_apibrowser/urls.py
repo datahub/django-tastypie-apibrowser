3d50ae6007e8be79400074dba7fb269fed53ffb5
Allan James Vestal <allanjamesvestal@gmail.com>
2014-02-17 19:32:13 -0600

    Bugfixes.

--- tastypie_apibrowser/urls.py
+++ tastypie_apibrowser/urls.py
@@ -4,14 +4,14 @@ from django.views.generic import TemplateView
 
 
 # Imports from this app
-from views import staff_member_template
+# from views import staff_member_template
 
 
 urlpatterns = patterns('',
-    (r'^$', staff_member_template, {'template_name':'apibrowser/index.html'},
-        'name=api-browser'),
-    (r'^templates$', TemplateView.as_view(
-            template_name='apibrowser/templates.html'),
-        'name=api-browser-templates'),
+    # (r'^$', staff_member_template, {'template_name':'apibrowser/index.html'},
+        # 'name=api-browser'),
+    # (r'^templates$', TemplateView.as_view(
+            # template_name='apibrowser/templates.html'),
+        # 'name=api-browser-templates'),
     )
 
